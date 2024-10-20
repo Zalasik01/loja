eaac024ba92179a59325e3a3951e6f1f597e46a2
Nicolas Zalasik <nizalasik@gmail.com>
2024-10-20 16:42:07 -0300

    destaque

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $('.featured-products').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '40px',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: '40px',
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                    centerPadding: '40px',
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: false,
+                    centerPadding: '0',
+                }
+            }
+        ]
+    });
+});
